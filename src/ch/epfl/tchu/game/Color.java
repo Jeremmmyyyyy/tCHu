dd5e8e3cba0adaf72b2647582dbe64ed20f61b19
Jeremmmyyyyy <79469048+Jeremmmyyyyy@users.noreply.github.com>
2021-02-22 17:57:22 +0100

    Update Color.java

--- src/ch/epfl/tchu/game/Color.java
+++ src/ch/epfl/tchu/game/Color.java
@@ -13,7 +13,6 @@ public enum Color {
     RED,
     WHITE;
 
-//    blalvblallfzgkubuora
 
     public static final List<Color> ALL = List.of(Color.values());
     public static final int COUNT = ALL.size();
