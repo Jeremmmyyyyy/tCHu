a283828b46789095b5e6ad3450d5aa9eac0f3f65
Jérémy Valentin Barghorn <jeremy.barghorn@epfl.ch>
2021-02-24 10:15:23 +0100

    StationConnectivity update

--- src/ch/epfl/tchu/game/StationConnectivity.java
+++ src/ch/epfl/tchu/game/StationConnectivity.java
@@ -1,6 +1,10 @@
 package ch.epfl.tchu.game;
 
 public interface StationConnectivity {
-
-    static
+    /**
+     * @param s1 Station number 1
+     * @param s2 Station number 2
+     * @return true if stations are connected by one player
+     */
+    boolean connected(Station s1, Station s2);
 }
