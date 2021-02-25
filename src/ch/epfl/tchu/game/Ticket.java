21e58ad9de1668e439f303854bf0bd52421c93bc
BuildTools <unconfigured@null.spigotmc.org>
2021-02-25 15:13:47 +0100

    Classe Ticket finie

--- src/ch/epfl/tchu/game/Ticket.java
+++ src/ch/epfl/tchu/game/Ticket.java
@@ -1,13 +1,12 @@
 package ch.epfl.tchu.game;
 
-import java.util.Collection;
-import java.util.Collections;
+import java.util.TreeSet;
 import java.util.List;
 import java.util.Objects;
 
 public final class Ticket implements Comparable<Ticket> {
 
-    private List<Trip> trips;
+    private final List<Trip> trips;
     private final String text;
 
     /**
@@ -16,7 +15,7 @@ public final class Ticket implements Comparable<Ticket> {
     public Ticket(List<Trip> trips){
         //TODO ajouter une deuxieme condition, avec IllegalArgumentException
         this.trips = Objects.requireNonNull(trips);
-        text = computeText();
+        text = computeText(this.trips);
     }
 
     /**
@@ -26,18 +25,33 @@ public final class Ticket implements Comparable<Ticket> {
      * @param points value of the trip
      */
     public Ticket(Station from, Station to, int points) { //TODO a revoir
+        this(List.of(new Trip(from, to, points)));
+    }
+
+    public String text(){
+        return text;
     }
 
     @Override
-    public int compareTo(Ticket ticket) {
-        return 0;
+    public int compareTo(Ticket that) {
+        return this.compareTo(that);
     }
 
-    private static String computeText(){
-
+    private static String computeText(List<Trip> trips) {
+        Trip firstTrip = trips.get(0);
+        String from = String.format("%s - ", firstTrip.getFrom().getName());
+        if (trips.size() == 1) {
+            return from + String.format("%s (%s)", firstTrip.getTo().getName(), firstTrip.getPoints());
+        }
+        TreeSet<String> orderedDestinations = new TreeSet<>();
+        for(Trip trip: trips){
+            orderedDestinations.add(String.format("%s (%s)", trip.getTo().getName(), trip.getPoints()));
+        }
+        return from + String.format("{%s}", String.join(", ",orderedDestinations));
     }
-    public String text(){
+
+    @Override
+    public String toString(){
         return text;
-
     }
 }
