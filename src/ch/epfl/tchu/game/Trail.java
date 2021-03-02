9212598e0129e3437304a0462d922a672ebb812c
BuildTools <unconfigured@null.spigotmc.org>
2021-03-02 12:39:33 +0100

    Ajout commentaires Trail

--- src/ch/epfl/tchu/game/Trail.java
+++ src/ch/epfl/tchu/game/Trail.java
@@ -13,17 +13,41 @@ public final class  Trail {
     private Station station1;
     private Station station2;
 
+    /**
+     * Creates the longest trail with a given list of routes
+     * @param routes list of routes
+     * @return the longest trail constructed by the given routes
+     * If routes is empty, returns a 0-long trail, with station 1 and station2 null
+     * If they are multiple longest trails, returns an arbitrary one
+     */
     public static Trail longest(List<Route> routes){
         while(!routes.isEmpty()){
+            List<Route> temp1 = new ArrayList<>();
+            for (Route initialTrail : routes) {
+                List<Route> temp2 = new ArrayList<>();
+                for(Route prolongation : routes) {
+                    if();
+                    temp2.add(prolongation);
+                }
+            }
 
         }
         return null;
     }
 
+    /**
+     * Getter for the attribute length
+     * @return the attribute length
+     */
     public int length(){
         return length;
     }
 
+    /**
+     * Getter for the attribute station1
+     * @return the attribute station1
+     * else null if the station1 is null
+     */
     public Station station1(){
         if(station1.equals(null)){
             return null;
@@ -31,6 +55,11 @@ public final class  Trail {
         return station1;
     }
 
+    /**
+     * Getter for the attribute station2
+     * @return the attribute station2
+     * else null if the station2 is null
+     */
     public Station station2(){
         if(station2.equals(null)){
             return null;
