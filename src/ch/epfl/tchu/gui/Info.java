6c00b84463d340f59abf892714556d1d74b97bd6
BuildTools <unconfigured@null.spigotmc.org>
2021-03-09 13:23:51 +0100

    Ajout commentaire Deck et Info + fin Info

--- src/ch/epfl/tchu/gui/Info.java
+++ src/ch/epfl/tchu/gui/Info.java
@@ -3,22 +3,42 @@ package ch.epfl.tchu.gui;
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.Card;
 import ch.epfl.tchu.game.Route;
+import ch.epfl.tchu.game.Trail;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Global and specific textual information attached to a player
+ *
+ * @author Yann Ennassih (329978)
+ */
 public final class Info {
 
     private String playerName;
 
+    /**
+     * Constructor of the class Info //TODO constructeur prive ou public ???
+     * @param playerName of the given player attached to ingame information
+     */
     Info(String playerName) {
         this.playerName = playerName;
     }
 
+    /**
+     * Creates a textual representation of a route
+     * @param route to put as String
+     * @return "fromStation - toStation"
+     */
     private static String routeName(Route route) {
         return String.format("%s %s %s", route.station1(), StringsFr.EN_DASH_SEPARATOR, route.station2());
     }
 
+    /**
+     * Creates a textual representation of a set of cards
+     * @param cards to put as String
+     * @return "multiplicity1 card1, ... , multiplicityN cardN"
+     */
     private static String cardsName(SortedBag<Card> cards) {
         List<String> cardsName = new ArrayList<>();
         for (Card card : cards.toSet()) {
@@ -30,6 +50,11 @@ public final class Info {
                 String.join(", ", cardsName.subList(0, lastIndex)), cardsName.get(lastIndex));
     }
 
+    /**
+     * Returns the name of the given card
+     * @param card whose name is to return
+     * @return name of the card, including plural form
+     */
     public static String cardName(Card card, int count) {
         String cardName = new String();
         switch (card) {
@@ -64,18 +89,37 @@ public final class Info {
         return cardName + StringsFr.plural(count);
     }
 
+    /**
+     * Creates a message in case of a draw
+     * @param playerNames players of the game
+     * @param points of the draw (of both players)
+     * @return "playerName1 et playerName2 sont ex æqo avec points points !"
+     */
     public static String draw(List<String> playerNames, int points) {
         return String.format(StringsFr.DRAW, String.join(StringsFr.AND_SEPARATOR, playerNames), points);
     }
 
+    /**
+     * Displays that the player plays first
+     * @return "playerName jouera en premier."
+     */
     public String willPlayFirst() {
         return String.format(StringsFr.WILL_PLAY_FIRST, playerName);
     }
 
+    /**
+     * Displays the number of tickets kept by the player
+     * @param count number of tickets kept by the player
+     * @return "playerName a gardé count billet(s)."
+     */
     public String keptTickets(int count) {
         return String.format(StringsFr.KEPT_N_TICKETS, playerName, count);
     }
 
+    /**
+     *
+     * @return
+     */
     public String canPlayer() {
         return String.format(StringsFr.CAN_PLAY, playerName);
     }
@@ -112,6 +156,15 @@ public final class Info {
 
     public String lastTurnBegins(int carCount) {
         return String.format(StringsFr.LAST_TURN_BEGINS, playerName, carCount, StringsFr.plural(carCount));
+    }
+
+    public String getsLongestTrailBonus(Trail longestTrail) {
+        return String.format(StringsFr.GETS_BONUS, playerName, String.join(StringsFr.EN_DASH_SEPARATOR,
+                longestTrail.station1().name(),longestTrail.station2().name()));
+    }
+
+    public String won(int points, int loserPoints) {
+        return String.format(StringsFr.WINS, playerName, points, loserPoints);
     }
 
 }
