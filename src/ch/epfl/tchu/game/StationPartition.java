fad2b9a47383e093afae1556ff61d9a96db621b1
BuildTools <unconfigured@null.spigotmc.org>
2021-03-15 13:58:57 +0100

    Fin StationPartition

--- src/ch/epfl/tchu/game/StationPartition.java
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -1,19 +1,34 @@
 package ch.epfl.tchu.game;
 
-import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a partition of the stations given a player's connectivity
+ *
+ * @author Yann Ennassih (329978)
+ */
 public final class StationPartition implements StationConnectivity {
 
     private Map<Integer, Integer> stationPartition;
 
+    /**
+     * Private constructor for StationPartition
+     * @param stationPartition map where each station id (value) is associated to one lead-station id (key)
+     */
     private StationPartition(Map<Integer, Integer> stationPartition) {
         this.stationPartition = stationPartition;
     }
 
+    /**
+     * Tests if two stations are connected in the current station partition
+     * @param s1 Station number 1
+     * @param s2 Station number 2
+     * @return true if s1 and s2 are connected
+     * true else if stationPartition doesn't contain s1 or s2 and s1 == s2
+     * false else
+     */
     @Override
     public boolean connected(Station s1, Station s2) {
-
-        return false;
+        return stationPartition.getOrDefault(s1.id(), s1.id()) == stationPartition.getOrDefault(s2.id(), s2.id());
     }
 }
