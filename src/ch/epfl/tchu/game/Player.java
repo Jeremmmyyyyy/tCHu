cabfede9e2bdea8b2e79a7a8c6ae6fec10aa378f
Jeremmmyyyyy <79469048+Jeremmmyyyyy@users.noreply.github.com>
2021-03-22 14:18:59 +0100

    Player interface added

---
+++ src/ch/epfl/tchu/game/Player.java
@@ -0,0 +1,45 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.SortedBag;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Player {
+
+    /**
+     * Interface representing the tree different actions a player has during his turn
+     */
+    enum TurnKind{
+        DRAW_TICKETS,
+        DRAW_CARDS,
+        CLAIM_ROUTE;
+
+        /**
+         * List containing all the options a player have during his turn
+         **/
+        public static final List<TurnKind> ALL = List.of(TurnKind.values());
+    }
+
+    void initPlayers(PlayerId ownId, Map<PlayerId, String> playerNames);
+
+    void receiveInfo(String info);
+
+    void updateState(PublicGameState newState, PlayerState ownState);
+
+    void setInitialTicketChoice(SortedBag<Ticket> tickets);
+
+    SortedBag<Ticket> chooseInitialTickets();
+
+    TurnKind nextTurn();
+
+    SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options);
+
+    int drawSlot();
+
+    Route claimedRoute();
+
+    SortedBag<Card> initialClaimCards();
+
+    SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options);
+}
