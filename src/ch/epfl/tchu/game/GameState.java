510bcf1edb12c181a1aafab40ebca6f9f962e9fe
BuildTools <unconfigured@null.spigotmc.org>
2021-03-22 14:20:08 +0100

    Ajout GameState

---
+++ src/ch/epfl/tchu/game/GameState.java
@@ -0,0 +1,57 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.Preconditions;
+import ch.epfl.tchu.SortedBag;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Random;
+
+public final class GameState extends PublicGameState {
+
+    private final Map<PlayerId, PlayerState> playerState;
+    private final CardState cardState;
+    private final Deck tickets;
+
+    private GameState(Deck tickets, CardState cardState, PlayerId currentPlayerId, Map<PlayerId,
+            PlayerState> playerState, PlayerId lastPlayer) {
+        super(tickets.size(), cardState, currentPlayerId, Map.copyOf(playerState), lastPlayer); //TODO revoir la copie de map
+        this.playerState = playerState;
+        this.cardState = cardState;
+        this.tickets = tickets; //TODO revoir rng = null
+    }
+
+    public static GameState initial(SortedBag<Ticket> tickets, Random rng) {
+        Deck<Card> initialDeck = Deck.of(Constants.ALL_CARDS, rng);
+        SortedBag<Card> initialPlayer1Cards = initialDeck.topCards(4);
+        SortedBag<Card> initialPlayer2Cards = initialDeck.withoutTopCards(4).topCards(4);
+        CardState initialCardState = CardState.of(initialDeck.withoutTopCards(8));
+        Deck<Ticket> initialTickets = Deck.of(tickets, rng);
+        Map<PlayerId, PlayerState> initialPlayerState = new EnumMap<>(PlayerId.class);
+        initialPlayerState.put(PlayerId.PLAYER_1, PlayerState.initial(initialPlayer1Cards));
+        initialPlayerState.put(PlayerId.PLAYER_2, PlayerState.initial(initialPlayer2Cards));
+        PlayerId initialPlayer = PlayerId.ALL.get(rng.nextInt(2));
+        return new GameState(initialTickets, initialCardState, initialPlayer, initialPlayerState, initialPlayer); //TODO A REVOIR
+
+    }
+
+    @Override
+    public PlayerState playerState(PlayerId playerId) {
+        return playerState.get(playerId);
+    }
+
+    @Override
+    public PlayerState currentPlayerState() {
+        return playerState.get(currentPlayerId());
+    }
+
+    public SortedBag<Ticket> topTickets(int count) {
+        Preconditions.checkArgument( count >= 0 && count <= tickets.size());
+        return tickets.topCards(count);
+    }
+
+    public GameState withoutTopTickets(int count) {
+        return new GameState(tickets, cardState, currentPlayerId(), playerState, lastPlayer());
+    }
+
+}
