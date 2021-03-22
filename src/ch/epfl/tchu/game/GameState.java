039bdd0d45f73a34f85d963659b16937ce6c04c3
BuildTools <unconfigured@null.spigotmc.org>
2021-03-22 15:06:32 +0100

    Ajout d'autres methodes de GameState

--- src/ch/epfl/tchu/game/GameState.java
+++ src/ch/epfl/tchu/game/GameState.java
@@ -25,6 +25,7 @@ public final class GameState extends PublicGameState {
     public static GameState initial(SortedBag<Ticket> tickets, Random rng) {
         Deck<Card> initialDeck = Deck.of(Constants.ALL_CARDS, rng);
         SortedBag<Card> initialPlayer1Cards = initialDeck.topCards(4);
+        //TODO A CHECK COMMENT DISTRIBUER LES PREMIERES CARTES ET DONC INITIALISER LE PLAYERSTATE
         SortedBag<Card> initialPlayer2Cards = initialDeck.withoutTopCards(4).topCards(4);
         CardState initialCardState = CardState.of(initialDeck.withoutTopCards(8));
         Deck<Ticket> initialTickets = Deck.of(tickets, rng);
@@ -47,22 +48,22 @@ public final class GameState extends PublicGameState {
     }
 
     public SortedBag<Ticket> topTickets(int count) {
-        Preconditions.checkArgument( count >= 0 && count <= tickets.size());
+        Preconditions.checkArgument( count >= 0 && count <= tickets.size()); //TODO NECESSAIRE ??????
         return tickets.topCards(count);
     }
 
     public GameState withoutTopTickets(int count) {
-        Preconditions.checkArgument(count >= 0 && count <= tickets.size());
+        Preconditions.checkArgument(count >= 0 && count <= tickets.size()); //TODO NECESSAIRE ??????
         return new GameState(tickets.withoutTopCards(count), cardState, currentPlayerId(), playerState, lastPlayer());
     }
 
     public Card topCard() {
-        Preconditions.checkArgument(cardState.isDeckEmpty());
+        Preconditions.checkArgument(!cardState.isDeckEmpty());  //TODO NECESSAIRE ??????
         return cardState.topDeckCard();
     }
 
     public GameState withoutTopCard() {
-        Preconditions.checkArgument(cardState.isDeckEmpty());
+        Preconditions.checkArgument(!cardState.isDeckEmpty()); //TODO NECESSAIRE ??????
         return new GameState(tickets, cardState.withoutTopDeckCard(), currentPlayerId(), playerState, lastPlayer());
     }
 
@@ -72,11 +73,29 @@ public final class GameState extends PublicGameState {
     }
 
     public GameState withCardsDeckRecreatedIfNeeded(Random rng) {
+        //TODO VOIR LEXCEPTION THROWN PAR LA METHODE DE CARDSTATE
         if (cardState.isDeckEmpty()) {
             return new GameState(tickets, cardState, currentPlayerId(), playerState, lastPlayer());
         }
         return new GameState(tickets, cardState.withDeckRecreatedFromDiscards(rng),
                 currentPlayerId(), playerState, lastPlayer());
+    }
+
+    public GameState withInitiallyChosenTickets(PlayerId playerId, SortedBag<Ticket> chosenTickets) {
+        for (Ticket chosenTicket: chosenTickets) {
+            Preconditions.checkArgument(playerState.get(playerId).tickets().contains(chosenTicket));
+        }
+        Map<PlayerId, PlayerState> newPlayerState = new EnumMap<>(playerState); //TODO CONSTRUCTEUR DE COPIE ?????
+        newPlayerState.replace(currentPlayerId(), playerState.get(playerId).withAddedTickets(chosenTickets));
+        return new GameState(tickets, cardState, currentPlayerId(), newPlayerState, lastPlayer());
+    }
+
+    public boolean lastTurnBegins() {
+        return false; //TODO a faire
+    }
+
+    public GameState forNextTurn() {
+        return new GameState(tickets, cardState, lastPlayer(), playerState, lastPlayer());
     }
 
 
