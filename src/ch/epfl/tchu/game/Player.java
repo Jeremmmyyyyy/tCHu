2cc0988bbd41b07597d3a12b476f31b82f0f7366
Jeremmmyyyyy <79469048+Jeremmmyyyyy@users.noreply.github.com>
2021-03-24 23:23:37 +0100

    comments et tests

--- src/ch/epfl/tchu/game/Player.java
+++ src/ch/epfl/tchu/game/Player.java
@@ -25,25 +25,73 @@ public interface Player {
         public static final List<TurnKind> ALL = List.of(TurnKind.values());
     }
 
+    /**
+     * Called at the beginning of the game to communicate his Id to the player and the playerNames of the other players
+     * @param ownId Id of the player
+     * @param playerNames names of all the players
+     */
     void initPlayers(PlayerId ownId, Map<PlayerId, String> playerNames);
 
+    /**
+     * Called each time an information is communicated to a player during the game
+     * @param info that ahs to be transmitted : generally created with Info.java
+     */
     void receiveInfo(String info);
 
+    /**
+     * Called each time the GameState changes
+     * @param newState new PublicGameState that has to be known by the player
+     * @param ownState PlayerState
+     */
     void updateState(PublicGameState newState, PlayerState ownState);
 
+    /**
+     * Called at the beginning of the game to communicate the 5 tickets to the player
+     * @param tickets SortedBag containing all the 5 drawn tickets
+     */
     void setInitialTicketChoice(SortedBag<Ticket> tickets);
 
+    /**
+     * Called at the beginning of the game : ask the player which 3 tickets he wants
+     * @return the 3 chosen tickets
+     */
     SortedBag<Ticket> chooseInitialTickets();
 
+    /**
+     * Called at the beginning of a turn to know which action the player wants to perform
+     * @return the action the player wants to play during his turn
+     */
     TurnKind nextTurn();
 
-    SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options);
+    /**
+     * Called if the player wants to draw additional billets
+     * @param options
+     * @return
+     */
+    SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options);//TODO billets ???
 
+    /**
+     * Called if the player wants to draw Cards
+     * @return 0-4 if the card is a faceUpCard or Constants.DECK_SLOT(-1) if drawn from the deck
+     */
     int drawSlot();
 
+    /**
+     * Called if the player try to take control of a route
+     * @return the route that the player try to control
+     */
     Route claimedRoute();
 
+    /**
+     * Called if the player try to take a route and ask him for the initial cards
+     * @return the cards that are given by the player
+     */
     SortedBag<Card> initialClaimCards();
 
+    /**
+     * Called if a player try to take control of a tunnel and additional cards are required
+     * @param options list of the possible additional cards combinations (empty if the player can't control or don't want to control the route
+     * @return a SortedBag
+     */
     SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options);
 }
