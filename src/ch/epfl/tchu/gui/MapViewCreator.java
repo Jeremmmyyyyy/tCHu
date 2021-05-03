e39ae78aa25d3e82a02a2c95f0a646477abdbdd1
BuildTools <unconfigured@null.spigotmc.org>
2021-05-03 13:52:02 +0200

    ajout actionhandlers

--- src/ch/epfl/tchu/gui/MapViewCreator.java
+++ src/ch/epfl/tchu/gui/MapViewCreator.java
@@ -1,8 +1,10 @@
 package ch.epfl.tchu.gui;
 
+import javafx.beans.property.ObjectProperty;
+
 class MapViewCreator {
 
-    public void createMapView(ObservableGameState observableGameState) {
+    public void createMapView(ObservableGameState observableGameState, ObjectProperty<ActionHandlers.ClaimRouteHandler> ) {
 
     }
 
