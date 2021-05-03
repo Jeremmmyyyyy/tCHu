29612901f2e940e6bedce6399021b965958da722
Jeremmmyyyyy <79469048+Jeremmmyyyyy@users.noreply.github.com>
2021-05-03 15:06:50 +0200

    deckview

--- src/ch/epfl/tchu/gui/DecksViewCreator.java
+++ src/ch/epfl/tchu/gui/DecksViewCreator.java
@@ -1,11 +1,20 @@
 package ch.epfl.tchu.gui;
 
+import ch.epfl.tchu.game.ChMap;
+import ch.epfl.tchu.game.Ticket;
 import javafx.application.Application;
+import javafx.beans.Observable;
 import javafx.beans.property.ObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import org.w3c.dom.Node;
+
 
 final class DecksViewCreator {
 
@@ -17,11 +26,27 @@ final class DecksViewCreator {
         HBox hBoxTickets = new HBox();
         hBoxTickets.setId("hand-pane");
 
-        ListView listView = new ListView();
+        ObservableList<Ticket> observableList = FXCollections.unmodifiableObservableList(FXCollections.observableList(ChMap.tickets()));
+        ListView<Ticket> listView = new ListView<Ticket>(observableList);
         listView.setId("tickets");
 
+
         hBoxMain.getChildren().addAll(hBoxTickets, listView);
-        return null;
+
+        Rectangle rectangleOutside = new Rectangle();
+        rectangleOutside.getStyleClass().add("outside");
+        Rectangle rectangleInside = new Rectangle();
+        rectangleInside.getStyleClass().addAll("filled", "inside");
+        Rectangle rectangleImage = new Rectangle();
+        rectangleImage.getStyleClass().add("train-image");
+        Text counter = new Text();
+        counter.getStyleClass().add("count");
+
+        StackPane stackPaneBlack = new StackPane();
+        stackPaneBlack.getStyleClass().addAll("BLACK", "card");
+        stackPaneBlack.getChildren().addAll(rectangleOutside, rectangleInside, rectangleImage, counter);
+
+        return hBoxMain;
     }
 
     public Node createCardsView(ObservableGameState observableGameState,
