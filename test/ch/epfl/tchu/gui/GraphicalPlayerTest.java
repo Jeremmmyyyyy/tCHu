9405d8dc3624b56db7e4465a2817e543bafbbe72
Jeremmmyyyyy <79469048+Jeremmmyyyyy@users.noreply.github.com>
2021-05-14 18:40:41 +0200

    c'est pas ouf

--- test/ch/epfl/tchu/gui/GraphicalPlayerTest.java
+++ test/ch/epfl/tchu/gui/GraphicalPlayerTest.java
@@ -2,62 +2,69 @@ package ch.epfl.tchu.gui;
 
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.ChMap;
 import ch.epfl.tchu.game.PlayerId;
-import ch.epfl.tchu.gui.ActionHandlers.ClaimRouteHandler;
-import ch.epfl.tchu.gui.ActionHandlers.DrawCardHandler;
-import ch.epfl.tchu.gui.ActionHandlers.DrawTicketsHandler;
+import ch.epfl.tchu.game.PlayerState;
+import ch.epfl.tchu.game.PublicCardState;
+import ch.epfl.tchu.game.PublicGameState;
+import ch.epfl.tchu.game.PublicPlayerState;
+import ch.epfl.tchu.game.Ticket;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.junit.jupiter.api.Test;
 
-import javax.swing.text.html.ListView;
-
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static ch.epfl.tchu.game.PlayerId.PLAYER_1;
 import static ch.epfl.tchu.game.PlayerId.PLAYER_2;
-import static org.junit.jupiter.api.Assertions.*;
 
-class GraphicalPlayerTest extends Application {
-
-    @Test
-    void toStringWorks() {
-        GraphicalPlayer.CardBagStringConverter c = new GraphicalPlayer.CardBagStringConverter();
-        SortedBag<Card> a = SortedBag.of(1, Card.VIOLET, 3, Card.RED);
-        SortedBag<Card> b = SortedBag.of(2, Card.BLUE, 1, Card.LOCOMOTIVE);
-        SortedBag<Card> d = SortedBag.of(1, Card.BLACK, 1, Card.GREEN);
-        SortedBag<Card> e = SortedBag.of(1, Card.VIOLET);
-        SortedBag<Card> f = SortedBag.of(3, Card.VIOLET);
-        assertEquals("1 violette et 3 rouges", c.toString(a));
-        assertEquals("2 bleues et 1 locomotive", c.toString(b));
-        assertEquals("1 noire et 1 verte", c.toString(d));
-        assertEquals("1 violette", c.toString(e));
-        assertEquals("3 violettes", c.toString(f));
-    }
-
+public final class GraphicalPlayerTest extends Application {
     private void setState(GraphicalPlayer player) {
         // … construit exactement les mêmes états que la méthode setState
         // du test de l'étape 9
+
+        PlayerState p1State =
+                new PlayerState(SortedBag.of(ChMap.tickets().subList(0, 3)),
+                        SortedBag.of(4, Card.WHITE, 4, Card.RED),
+                        ChMap.routes().subList(0, 3));
+
+        PublicPlayerState p2State =
+                new PublicPlayerState(0, 0, ChMap.routes().subList(3, 6));
+
+        Map<PlayerId, PublicPlayerState> pubPlayerStates =
+                Map.of(PLAYER_1, p1State, PLAYER_2, p2State);
+        PublicCardState cardState =
+                new PublicCardState(Card.ALL.subList(0, 5), 110 - 2 * 4 - 5, 0);
+        PublicGameState publicGameState =
+                new PublicGameState(36, cardState, PLAYER_1, pubPlayerStates, null);
+
         player.setState(publicGameState, p1State);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         Map<PlayerId, String> playerNames =
                 Map.of(PLAYER_1, "Ada", PLAYER_2, "Charles");
         GraphicalPlayer p = new GraphicalPlayer(PLAYER_1, playerNames);
         setState(p);
 
-        DrawTicketsHandler drawTicketsH =
+        ActionHandlers.DrawTicketsHandler drawTicketsH =
                 () -> p.receiveInfo("Je tire des billets !");
-        DrawCardHandler drawCardH =
+        ActionHandlers.DrawCardHandler drawCardH =
                 s -> p.receiveInfo(String.format("Je tire une carte de %s !", s));
-        ClaimRouteHandler claimRouteH =
+        ActionHandlers.ClaimRouteHandler claimRouteH =
                 (r, cs) -> {
                     String rn = r.station1() + " - " + r.station2();
                     p.receiveInfo(String.format("Je m'empare de %s avec %s", rn, cs));
                 };
 
         p.startTurn(drawTicketsH, drawCardH, claimRouteH);
+
+        //  p.chooseTickets(SortedBag.of(ChMap.tickets().subList(0,5)), s-> p.receiveInfo(String.valueOf(s.size())));
+
+        List<SortedBag<Card>> cards = List.of(SortedBag.of(2, Card.RED), SortedBag.of(2, Card.BLUE), SortedBag.of(4, Card.BLACK));
+        p.chooseAdditionalCards(cards, s-> p.receiveInfo(String.valueOf(s.size())));
+
     }
 }
