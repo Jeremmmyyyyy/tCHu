d1fe9e5816ccfddc795f118fe2ba98dd912f9108
BuildTools <unconfigured@null.spigotmc.org>
2021-05-17 17:02:17 +0200

    ServerMain et ClientMain premier jet

---
+++ src/ch/epfl/tchu/gui/ClientMain.java
@@ -0,0 +1,21 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.net.RemotePlayerClient;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public abstract class ClientMain extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        String hostName = getParameters().getRaw().get(0);
+        int port = Integer.parseInt(getParameters().getRaw().get(1));
+
+        GraphicalPlayerAdapter graphicalPlayer = new GraphicalPlayerAdapter();
+        RemotePlayerClient distantClient = new RemotePlayerClient(graphicalPlayer, hostName, port);
+
+        new Thread(distantClient::run);
+    }
+}
