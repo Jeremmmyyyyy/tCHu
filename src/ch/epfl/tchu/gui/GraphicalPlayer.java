59b6a02148cf0eb3b87b3464c8f28d56aad12334
BuildTools <unconfigured@null.spigotmc.org>
2021-05-28 00:59:34 +0200

    InfoViewCreator and GraphicalPlayer tout good aussi

--- src/ch/epfl/tchu/gui/GraphicalPlayer.java
+++ src/ch/epfl/tchu/gui/GraphicalPlayer.java
@@ -24,15 +24,17 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.StringConverter;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
 import static ch.epfl.tchu.game.Constants.*;
 import static ch.epfl.tchu.gui.StringsFr.*;
 import static javafx.application.Platform.isFxApplicationThread;
 
 /**
- * GraphicalPlayer that creates the whole GUI for the players
+ * GraphicalPlayer that creates the whole GUI for a player
  * All the different possible actions are called on the javaFX thread
  *
  * @author Yann Ennassih (329978)
@@ -66,6 +68,7 @@ public final class GraphicalPlayer {
         drawCardHandlerProperty = new SimpleObjectProperty<>();
         claimRouteHandlerProperty = new SimpleObjectProperty<>();
 
+        //Creates the different nodes of the gui
         Node mapView = MapViewCreator
                 .createMapView(observableGameState, claimRouteHandlerProperty, this::chooseClaimCards);
         Node cardsView = DecksViewCreator
@@ -80,21 +83,20 @@ public final class GraphicalPlayer {
         mainStage.setTitle("tCHu \u2014 " + playerNames.get(playerId));
 
         mainStage.show();
-
     }
 
     /**
      * Calls the method of the same name in ObservableGameState
-     * @param publicGameState that updates the old one
-     * @param playerState that updates the old one
-     */
+     * @param publicGameState used to update the observableGameState
+     * @param playerState used to update the observableGameState
+    */
     public void setState(PublicGameState publicGameState, PlayerState playerState){
         assert isFxApplicationThread();
         observableGameState.setState(publicGameState, playerState);
     }
 
     /**
-     * Add the given message to the InfoView displays only the 5 last messages
+     * Adds the given message to the InfoView which display only the 5 last messages
      * @param message new message to add
      */
     public void receiveInfo(String message){
@@ -107,25 +109,29 @@ public final class GraphicalPlayer {
 
     /**
      * Is called at the beginning of a turn of a player so he can choose the TurnKind
-     * Different ActionHandlers are given in argument so that for each TurnKind the right Handler is called and complete the authorised actions
-     * @param drawTicketsHandler to handle what happens when the TurnKind DRAW_TICKETS is called
-     * @param drawCardHandler to handle what happens when the TurnKind DRAW_CARDS is called
-     * @param claimRouteHandler to handle what happens when the TurnKind CLAIM_ROUTE is called
+     * Different ActionHandlers are given so that for each TurnKind the right Handler
+     * is called and completes the authorised actions
+     * @param drawTicketsHandler to handle a DRAW_TICKETS call
+     * @param drawCardHandler to handle a DRAW_CARDS call
+     * @param claimRouteHandler to handle a CLAIM_ROUTE call
      */
     public void startTurn(DrawTicketsHandler drawTicketsHandler, DrawCardHandler drawCardHandler,
                           ClaimRouteHandler claimRouteHandler){
         assert isFxApplicationThread();
 
-        drawCardHandlerProperty.set(observableGameState.canDrawCards() ? drawSlot -> {
-            drawCardHandler.onDrawCard(drawSlot);
-            clearHandlerProperties();
-        } : null);
-
+        //TurnKind = DRAW_TICKETS
         drawTicketsHandlerProperty.set(observableGameState.canDrawTickets() ? () -> {
             drawTicketsHandler.onDrawTickets();
             clearHandlerProperties();
         } : null);
 
+        //TurnKind = DRAW_CARDS
+        drawCardHandlerProperty.set(observableGameState.canDrawCards() ? drawSlot -> {
+            drawCardHandler.onDrawCard(drawSlot);
+            clearHandlerProperties();
+        } : null);
+
+        //TurnKind = CLAIM_ROUTE (always filled)
         claimRouteHandlerProperty.set((route, cards) -> {
             claimRouteHandler.onClaimRoute(route, cards);
             clearHandlerProperties();
@@ -133,6 +139,10 @@ public final class GraphicalPlayer {
 
     }
 
+    /**
+     * Method called above, used to set all handlerProperties to null in order to prevent the player from playing an
+     * other additional action
+     */
     private void clearHandlerProperties() {
         drawTicketsHandlerProperty.set(null);
         drawCardHandlerProperty.set(null);
@@ -140,31 +150,29 @@ public final class GraphicalPlayer {
     }
 
     /**
-     * Takes a List of tickets and pop-ups a window so the player can choose the wished Tickets
+     * Takes a List of tickets and pop-ups a window so that the player can choose the wished Tickets
      * @param tickets SortedBag of tickets (5 at the beginning of the game and 3 otherwise)
      * @param chooseTicketsHandler ActionHandler that handles which tickets are chosen
      */
     public void chooseTickets(SortedBag<Ticket> tickets, ChooseTicketsHandler chooseTicketsHandler){
         assert isFxApplicationThread();
-
         Preconditions.checkArgument(tickets.size() == INITIAL_TICKETS_COUNT ||
                 tickets.size() == IN_GAME_TICKETS_COUNT);
 
+        //Arguments needed for stageCreator(...)
         ListView<Ticket> ticketsView = new ListView<>(FXCollections.observableList(tickets.toList()));
-
         Button button = new Button(CHOOSE);
+        int minimumDrawCount = tickets.size() - DISCARDABLE_TICKETS_COUNT;
 
-        int ticketsSize = tickets.size();
-
+        //Creates the choiceStage
         Stage choiceStage = stageCreator(
-                new Text(String.format(CHOOSE_TICKETS, ticketsSize - DISCARDABLE_TICKETS_COUNT,
-                        plural(ticketsSize - DISCARDABLE_TICKETS_COUNT))), button, ticketsView);
+                new Text(String.format(CHOOSE_TICKETS, minimumDrawCount,
+                        plural(minimumDrawCount))), button, ticketsView);
 
+        //Specific to chooseTickets(...)
         choiceStage.setTitle(TICKETS_CHOICE);
-
-        button.disableProperty().bind(Bindings.lessThan(
-                    Bindings.size(ticketsView.getSelectionModel().getSelectedItems()),
-                    ticketsSize - Constants.DISCARDABLE_TICKETS_COUNT));
+        button.disableProperty().bind(
+                Bindings.lessThan(Bindings.size(ticketsView.getSelectionModel().getSelectedItems()), minimumDrawCount));
         button.setOnAction(e -> {
             choiceStage.hide();
             chooseTicketsHandler.onChooseTickets(SortedBag.of(ticketsView.getSelectionModel().getSelectedItems()));
@@ -173,7 +181,8 @@ public final class GraphicalPlayer {
     }
 
     /**
-     * Authorise the player to draw a card from the deck (button "cartes") or from the FaceUpCards
+     * Authorises the player to draw a card from the deck (button "Cartes") or from the FaceUpCards
+     * Especially called on the second obligatory draw
      * @param drawCardHandler ActionHandler that handles which cards from which slot are chosen
      */
     public void drawCard(DrawCardHandler drawCardHandler){
@@ -183,19 +192,18 @@ public final class GraphicalPlayer {
             drawCardHandler.onDrawCard(drawSlot);
             clearHandlerProperties();
         });
-
     }
 
     /**
-     * Pop-ups a window with the different card possibilities to take a route
-     * @param initialClaimCards list of the different possible cards that can take the route
+     * Pop-ups a window with the different card possibilities to claim a route
+     * @param initialClaimCards list of the different possible cards that can claim the route
      * @param chooseCardsHandler ActionHandler that handles which Card possibility is used to take the route
      */
     public void chooseClaimCards(List<SortedBag<Card>> initialClaimCards, ChooseCardsHandler chooseCardsHandler){
         assert isFxApplicationThread();
 
+        //Selection button can't be clicked without an empty selection, thus "true" argument
         createCardsStage(true, new Text(CHOOSE_CARDS), initialClaimCards, chooseCardsHandler);
-
     }
 
     /**
@@ -207,10 +215,18 @@ public final class GraphicalPlayer {
                                       ChooseCardsHandler chooseCardsHandler){
         assert isFxApplicationThread();
 
+        //Selection button can be clicked without any choice selected, thus "false" argument
         createCardsStage(false, new Text(CHOOSE_ADDITIONAL_CARDS), possibleAdditionalCards, chooseCardsHandler);
     }
 
-
+    /**
+     * Method called by chooseTickets(...) or createCardsStage(...), used to create a generic stage
+     * @param text name of the selection box
+     * @param button choiceButton
+     * @param listView options on selection
+     * @param <E> parameter type (Ticket or SortedBag<Card>)
+     * @return a new generic stage for multiple selection window
+     */
     private <E> Stage stageCreator(Text text, Button button, ListView<E> listView) {
 
         Stage choiceStage = new Stage(StageStyle.UTILITY);
@@ -235,33 +251,36 @@ public final class GraphicalPlayer {
     private void createCardsStage(boolean buttonCanBeDisabled, Text text, List<SortedBag<Card>> cards,
                                   ChooseCardsHandler chooseCardsHandler) {
 
+        //Arguments needed for stageCreator
         ListView<SortedBag<Card>> cardsView = new ListView<>(FXCollections.observableList(cards));
         cardsView.setCellFactory(v -> new TextFieldListCell<>(new CardBagStringConverter()));
-
         Button button = new Button(CHOOSE);
 
+        //Creates the choiceStage
         Stage choiceStage = stageCreator(text, button, cardsView);
 
+        //Specific to chooseClaimCards(...) and chooseAdditionalCards(...)
         choiceStage.setTitle(CARDS_CHOICE);
-
         if (buttonCanBeDisabled) {
+            //Specific to chooseAdditionalCards : empty selection means the player didn't want to play additional cards
             button.disableProperty().bind(Bindings.isEmpty(cardsView.getSelectionModel().getSelectedItems()));
         }
         button.setOnAction(e -> {
             choiceStage.hide();
             SortedBag<Card> selectedCards = cardsView.getSelectionModel().getSelectedItem();
+            //Handles an empty sortedBag in case of an empty selection
             chooseCardsHandler.onChooseCards(selectedCards == null ? SortedBag.of() : SortedBag.of(selectedCards));
         });
 
     }
 
     /**
-     * Nested Class that displays a convenient StringFormat for a SortedBag
+     * Nested class that displays a convenient StringFormat for a SortedBag
      */
     private static final class CardBagStringConverter extends StringConverter<SortedBag<Card>> {
 
         /**
-         * toString method for a sortedBag
+         * Overridden toString method for a sortedBag of Card
          * @param object SortedBag of Cards to display in String format
          * @return String for the given SortedBag
          */
@@ -279,10 +298,10 @@ public final class GraphicalPlayer {
         }
 
         /**
-         * fromString method that throw UnsupportedOperationException when called. The inverse operation of toString is not used in our case so we don't want to implement it
+         * Overridden fromString method that throws UnsupportedOperationException when called.
+         * The inverse operation of toString is not used in this project so it has no specific implementation
          * @param string to convert into a SortedBag
-         * @return UnsupportedOperationException
-         * @throws UnsupportedOperationException
+         * @return throws an UnsupportedOperationException
          */
         @Override
         public SortedBag<Card> fromString(String string) {
--- src/ch/epfl/tchu/gui/InfoViewCreator.java
+++ src/ch/epfl/tchu/gui/InfoViewCreator.java
@@ -11,51 +11,49 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import static ch.epfl.tchu.game.PlayerId.PLAYER_1;
+import static ch.epfl.tchu.game.PlayerId.PLAYER_2;
 
 /**
- * Abstract class that creates the left panel of the GUI containing all the information of the Game
+ * Abstract class that creates the left panel of the GUI containing all the information of the Game (package private)
  *
  * @author Yann Ennassih (329978)
  * @author Jérémy Barghorn (328403)
  */
-abstract class InfoViewCreator {
+abstract class InfoViewCreator { //TODO abstract suffit pour non instanciable ou mettre des constructeurs prives vide ?
 
     private static final int CIRCLE_RADIUS = 5;
 
     /**
-     * Create a Node that corresponds to the left panel of the game
-     * @param playerId of the current player
+     * Creates a Node that corresponds to the left panel of the game
      * @param playerNames that maps the PlayerId to the names
-     * @param observableGameState current observableGameState so the points and other informations can be updated
+     * @param observableGameState current observableGameState so that the points and other information can be updated
      * @param gameMessages ObservableList containing the last 5 actions of the game
-     * @return a VBox corresponding to the left panel of the game
+     * @return a VBox corresponding to the left panel of the game, for the game global information
      */
     public static Node createInfoView(Map<PlayerId, String> playerNames,
                                       ObservableGameState observableGameState, ObservableList<Text> gameMessages) {
-
         VBox vBox = new VBox();
         vBox.getStylesheets().addAll("info.css", "colors.css");
 
         Separator separator = new Separator();
         separator.setOrientation(Orientation.HORIZONTAL);
 
-        TextFlow messages = new TextFlow();
-        messages.setId("game-info");
-        messages.getChildren().addAll(gameMessages);
-        Bindings.bindContent(messages.getChildren(), gameMessages);
+        TextFlow gameInfo = new TextFlow();
+        gameInfo.setId("game-info");
+        Bindings.bindContent(gameInfo.getChildren(), gameMessages);
 
+        //Creates the two player information displays
         for (PlayerId id : PlayerId.ALL) {
 
             VBox playerVBox = new VBox();
             playerVBox.setId("player-stats");
 
-            TextFlow playerStats = new TextFlow();
             Circle circle = new Circle(CIRCLE_RADIUS);
+            circle.getStyleClass().add("filled");
+
             Text text = new Text();
             text.textProperty().bind(Bindings.format(StringsFr.PLAYER_STATS,
                     playerNames.get(id),
@@ -63,17 +61,16 @@ abstract class InfoViewCreator {
                     observableGameState.cardCounts(id),
                     observableGameState.carCounts(id),
                     observableGameState.claimPoints(id)));
-            circle.getStyleClass().add("filled");
 
-            playerStats.getStyleClass().add(id == PLAYER_1 ? "PLAYER_1" : "PLAYER_2");
+            TextFlow playerStats = new TextFlow();
+            playerStats.getStyleClass().add(id == PLAYER_1 ? PLAYER_1.name() : PLAYER_2.name());
             playerStats.getChildren().addAll(circle, text);
 
             playerVBox.getChildren().add(playerStats);
             vBox.getChildren().add(playerVBox);
         }
 
-        vBox.getChildren().addAll(separator, messages);
-
+        vBox.getChildren().addAll(separator, gameInfo);
 
         return vBox;
     }
