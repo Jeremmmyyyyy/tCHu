5abe85f6e686dce2bf81538cea393b3c83df2c12
BuildTools <unconfigured@null.spigotmc.org>
2021-05-27 15:25:20 +0200

    Serde et Serdes parfaits !!

--- src/ch/epfl/tchu/net/Serde.java
+++ src/ch/epfl/tchu/net/Serde.java
@@ -1,6 +1,7 @@
 package ch.epfl.tchu.net;
 
 import ch.epfl.tchu.SortedBag;
+
 import java.util.List;
 import java.util.function.Function;
 import java.util.regex.Pattern;
@@ -8,28 +9,30 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * Generic Interface used to create functions that Serialize/Deserialize objects into a String
+ * Generic Interface used to create functions that serialize/deserialize objects into a String
  *
  * @author Jérémy Barghorn (328403)
  * @author Yann Ennassih (329978)
  */
+
 public interface Serde <T> {
+
     /**
-     * Serialize an object in the form of a String
+     * Serializes an object in the form of a String
      * @param object to be serialized
      * @return the serialized String
      */
     String serialize(T object);
 
     /**
-     * Deserialize an object
+     * Deserializes a string
      * @param arg string to be deserialized
      * @return the deserialized object
      */
     T deserialize(String arg);
 
     /**
-     * Create a serializer/deserializer function
+     * Creates an instance of Serde with given serializing/deserializing functions
      * @param serializer the function used to serialize an object
      * @param deserializer the function used to deserialize the String
      * @param <T> type of the object to serialize
@@ -50,17 +53,17 @@ public interface Serde <T> {
     }
 
     /**
-     * Create a Serde of a list of given elements
-     * @param allValues List of the elements
+     * Creates a Serde of a list of given elements
+     * @param allValues List of all the elements
      * @param <T> type of the elements in the List
-     * @return a Serde for this List
+     * @return a Serde for this list
      */
     static <T> Serde<T> oneOf(List<T> allValues){
         return of(t -> String.valueOf(allValues.indexOf(t)), s -> allValues.get(Integer.parseInt(s)));
     }
 
     /**
-     * Create a Serde that is able to (de)serialize lists of values that are (de)serialized by the given serde
+     * Creates a Serde that is able to (de)serialize lists of values that are (de)serialized by the given serde
      * @param serde that serialize the list
      * @param separator that is put in between each element of the list
      * @param <T> type of the elements in the list
@@ -85,8 +88,9 @@ public interface Serde <T> {
         };
 
     }
+
     /**
-     * Create a Serde that is able to (de)serialize SortedBags of values that are (de)serialized by the given serde
+     * Creates a Serde that is able to (de)serialize SortedBags of values that are (de)serialized by the given serde
      * @param serde that serialize the SortedBag
      * @param separator that is put in between each element of the list
      * @param <T> type of the elements in the SortedBag
--- src/ch/epfl/tchu/net/Serdes.java
+++ src/ch/epfl/tchu/net/Serdes.java
@@ -2,10 +2,11 @@ package ch.epfl.tchu.net;
 
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.*;
-import ch.epfl.tchu.net.Serde;
 
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
@@ -16,6 +17,11 @@ import java.util.regex.Pattern;
  */
 public final class Serdes {
 
+    private final static String COMMA_SEPARATOR = ",";
+    private final static String SEMICOLON_SEPARATOR = ";";
+    private final static String COLON_SEPARATOR = ":";
+    private final static int SPLIT_LIMIT = -1;
+
     //In order to make the class non instantiable
     private Serdes(){}
 
@@ -24,17 +30,17 @@ public final class Serdes {
      */
     public static final Serde<Integer> INTEGER_SERDE = Serde.of(i -> Integer.toString(i), Integer::parseInt);
     /**
-     * Serde for the Strings (Serialize an object into a String in base 64, Deserialize an object from base 64)
+     * Serde for the Strings (Serializes an object into a String in base 64, Deserializes an object from base 64)
      */
-    public static final Serde<String> STRING_SERDE = Serde
-            .of(i -> Base64.getEncoder().encodeToString(i.getBytes(java.nio.charset.StandardCharsets.UTF_8)),
-                    s -> new String(Base64.getDecoder().decode(s), StandardCharsets.UTF_8));
+    public static final Serde<String> STRING_SERDE = Serde.of(
+            i -> Base64.getEncoder().encodeToString(i.getBytes(java.nio.charset.StandardCharsets.UTF_8)),
+            s -> new String(Base64.getDecoder().decode(s), StandardCharsets.UTF_8));
     /**
      * Serde for the Enum PLayerId
      */
     public static final Serde<PlayerId> PLAYER_ID_SERDE = Serde.oneOf(PlayerId.ALL);
     /**
-     * Serde for the enum TurnKind
+     * Serde for the Enum TurnKind
      */
     public static final Serde<Player.TurnKind> TURN_KIND_SERDE = Serde.oneOf(Player.TurnKind.ALL);
     /**
@@ -52,88 +58,96 @@ public final class Serdes {
     /**
      * Serde for the List of Strings, separated by ","
      */
-    public static final Serde<List<String>> LIST_STRING_SERDE = Serde.listOf(STRING_SERDE, ",");
+    public static final Serde<List<String>> LIST_STRING_SERDE = Serde.listOf(STRING_SERDE, COMMA_SEPARATOR);
     /**
      * Serde for the List of Cards, separated by ","
      */
-    public static final Serde<List<Card>> LIST_CARD_SERDE = Serde.listOf(CARD_SERDE, ",");
+    public static final Serde<List<Card>> LIST_CARD_SERDE = Serde.listOf(CARD_SERDE, COMMA_SEPARATOR);
     /**
      * Serde for the List of Routes, separated by ","
      */
-    public static final Serde<List<Route>> LIST_ROUTE_SERDE = Serde.listOf(ROUTE_SERDE, ",");
+    public static final Serde<List<Route>> LIST_ROUTE_SERDE = Serde.listOf(ROUTE_SERDE, COMMA_SEPARATOR);
     /**
      * Serde for the SortedBag of Cards, separated by ","
      */
-    public static final Serde<SortedBag<Card>> SORTED_BAG_CARD_SERDE = Serde.bagOf(CARD_SERDE, ",");
+    public static final Serde<SortedBag<Card>> SORTED_BAG_CARD_SERDE = Serde.bagOf(CARD_SERDE, COMMA_SEPARATOR);
     /**
      * Serde for the SortedBag of Tickets, separated by ","
      */
-    public static final Serde<SortedBag<Ticket>> SORTED_BAG_TICKET_SERDE = Serde.bagOf(TICKET_SERDE, ",");
+    public static final Serde<SortedBag<Ticket>> SORTED_BAG_TICKET_SERDE = Serde.bagOf(TICKET_SERDE, COMMA_SEPARATOR);
     /**
      * Serde for the List of SortedBag of Cards, separated by ";"
      */
-    public static final Serde<List<SortedBag<Card>>> LIST_SORTEDBAG_CARD_SERDE = Serde.listOf(SORTED_BAG_CARD_SERDE, ";");
+    public static final Serde<List<SortedBag<Card>>> LIST_SORTEDBAG_CARD_SERDE = Serde.listOf(SORTED_BAG_CARD_SERDE, SEMICOLON_SEPARATOR);
 
     /**
      * Serde of PublicCardState, separated by ";"
      */
-    public static final Serde<PublicCardState> PUBLIC_CARD_STATE_SERDE = Serde.
-            of(cardState -> String.join(
-                    ";",
+    public static final Serde<PublicCardState> PUBLIC_CARD_STATE_SERDE = Serde.of(
+
+            //Serializing function
+            cardState -> String.join(
+                    SEMICOLON_SEPARATOR,
                     LIST_CARD_SERDE.serialize(cardState.faceUpCards()),
                     INTEGER_SERDE.serialize(cardState.deckSize()),
                     INTEGER_SERDE.serialize(cardState.discardsSize())),
 
-                    string -> {
-                        String[] split = string.split(Pattern.quote(";"), -1);
-                        return new PublicCardState(
-                                LIST_CARD_SERDE.deserialize(split[0]),
-                                INTEGER_SERDE.deserialize(split[1]),
-                                INTEGER_SERDE.deserialize(split[2]));
-                    });
+            //Deserializing function
+            string -> {
+                    String[] split = string.split(Pattern.quote(SEMICOLON_SEPARATOR), SPLIT_LIMIT);
+                    return new PublicCardState(
+                            LIST_CARD_SERDE.deserialize(split[0]),
+                            INTEGER_SERDE.deserialize(split[1]),
+                            INTEGER_SERDE.deserialize(split[2]));});
 
     /**
      * Serde of PublicPlayerState, separated by ";"
      */
-    public static final Serde<PublicPlayerState> PUBLIC_PLAYER_STATE_SERDE = Serde.
-            of(playerState -> String.join(
-                    ";",
+    public static final Serde<PublicPlayerState> PUBLIC_PLAYER_STATE_SERDE = Serde.of(
+
+            //Serializing function
+            playerState -> String.join(
+                    SEMICOLON_SEPARATOR,
                     INTEGER_SERDE.serialize(playerState.ticketCount()),
                     INTEGER_SERDE.serialize(playerState.cardCount()),
                     LIST_ROUTE_SERDE.serialize(playerState.routes())),
 
-                    string -> {
-                        String[] split = string.split(Pattern.quote(";"), -1);
-                        return new PublicPlayerState(
-                                INTEGER_SERDE.deserialize(split[0]),
-                                INTEGER_SERDE.deserialize(split[1]),
-                                LIST_ROUTE_SERDE.deserialize(split[2]));
-                    });
+            //Deserializing function
+            string -> {
+                    String[] split = string.split(Pattern.quote(SEMICOLON_SEPARATOR), SPLIT_LIMIT);
+                    return new PublicPlayerState(
+                            INTEGER_SERDE.deserialize(split[0]),
+                            INTEGER_SERDE.deserialize(split[1]),
+                            LIST_ROUTE_SERDE.deserialize(split[2]));});
 
     /**
      * Serde of PlayerState, separated by ";"
      */
-    public static final Serde<PlayerState> PLAYER_STATE_SERDE = Serde.
-            of(playerState -> String.join(
-                    ";",
+    public static final Serde<PlayerState> PLAYER_STATE_SERDE = Serde.of(
+
+            //Serializing function
+            playerState -> String.join(
+                    SEMICOLON_SEPARATOR,
                     SORTED_BAG_TICKET_SERDE.serialize(playerState.tickets()),
                     SORTED_BAG_CARD_SERDE.serialize(playerState.cards()),
                     LIST_ROUTE_SERDE.serialize(playerState.routes())),
 
-                    string -> {
-                        String[] split = string.split(Pattern.quote(";"), -1);
-                        return new PlayerState(
-                                SORTED_BAG_TICKET_SERDE.deserialize(split[0]),
-                                SORTED_BAG_CARD_SERDE.deserialize(split[1]),
-                                LIST_ROUTE_SERDE.deserialize(split[2]));
-                    });
+            //Deserializing function
+            string -> {
+                    String[] split = string.split(Pattern.quote(SEMICOLON_SEPARATOR), SPLIT_LIMIT);
+                    return new PlayerState(
+                            SORTED_BAG_TICKET_SERDE.deserialize(split[0]),
+                            SORTED_BAG_CARD_SERDE.deserialize(split[1]),
+                            LIST_ROUTE_SERDE.deserialize(split[2]));});
 
     /**
      * Serde of PublicGameState, separated by ":"
      */
-    public static final Serde<PublicGameState> PUBLIC_GAME_STATE_SERDE = Serde.
-            of(gameState -> String.join(
-                    ":",
+    public static final Serde<PublicGameState> PUBLIC_GAME_STATE_SERDE = Serde.of(
+
+            //Serializing function
+            gameState -> String.join(
+                    COLON_SEPARATOR,
                     INTEGER_SERDE.serialize(gameState.ticketsCount()),
                     PUBLIC_CARD_STATE_SERDE.serialize(gameState.cardState()),
                     PLAYER_ID_SERDE.serialize(gameState.currentPlayerId()),
@@ -141,15 +155,15 @@ public final class Serdes {
                     PUBLIC_PLAYER_STATE_SERDE.serialize(gameState.playerState(PlayerId.PLAYER_2)),
                     gameState.lastPlayer() == null ? "" : PLAYER_ID_SERDE.serialize(gameState.lastPlayer())),
 
-                    string -> {
-                        String[] split = string.split(Pattern.quote(":"), -1);
-                        return new PublicGameState(
-                                INTEGER_SERDE.deserialize(split[0]),
-                                PUBLIC_CARD_STATE_SERDE.deserialize(split[1]),
-                                PLAYER_ID_SERDE.deserialize(split[2]),
-                                Map.of(PlayerId.PLAYER_1, PUBLIC_PLAYER_STATE_SERDE.deserialize(split[3]),
-                                        PlayerId.PLAYER_2, PUBLIC_PLAYER_STATE_SERDE.deserialize(split[4])),
-                                split[5].isEmpty() ? null : PLAYER_ID_SERDE.deserialize(split[5]));
-                    });
+            //Deserializing function
+            string -> {
+                    String[] split = string.split(Pattern.quote(COLON_SEPARATOR), SPLIT_LIMIT);
+                    return new PublicGameState(
+                            INTEGER_SERDE.deserialize(split[0]),
+                            PUBLIC_CARD_STATE_SERDE.deserialize(split[1]),
+                            PLAYER_ID_SERDE.deserialize(split[2]),
+                            Map.of(PlayerId.PLAYER_1, PUBLIC_PLAYER_STATE_SERDE.deserialize(split[3]),
+                                    PlayerId.PLAYER_2, PUBLIC_PLAYER_STATE_SERDE.deserialize(split[4])),
+                            split[5].isEmpty() ? null : PLAYER_ID_SERDE.deserialize(split[5]));});
 
 }
