816134658a10914a232be9efa2855bb6cd921668
BuildTools <unconfigured@null.spigotmc.org>
2021-06-02 17:10:47 +0200

    sauvegarde tutorial

--- src/ch/epfl/tchu/gui/TutorialViewCreator.java
+++ src/ch/epfl/tchu/gui/TutorialViewCreator.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
